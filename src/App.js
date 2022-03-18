df4ee5aa9b681bb4eb9d38826c748297dcdd1ca2
SamiNousiainen <n1nosa00@students.oamk.fi>
2022-03-18 23:39:28 +0200

    frontend muutoksia

--- src/App.js
+++ src/App.js
@@ -8,19 +8,73 @@ const URL = 'http://localhost/shoppinglist';
 function App() {
 
   const [items, setItems] = useState([]);
+  const [item, setItem] = useState('');
+  const [amount, setAmount] = useState('');
 
   useEffect(() => {
     axios.get(URL)
     .then((response) => {
-
+        setItems(response.data);
     }).catch(error => {
-
-    })
+     alert(error.response ? error.response.data.error : error);
+    });
   }, [])
 
-  return (
-    <div>
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({ description: item, amount: amount });
+    axios.post(URL + 'add.php', json, {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((response) => {
+        setItems(items => [...items, response.data]);
+        setItem('');
+        setAmount('');
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error)
+      });
+  }
 
+  function remove(id) {
+    const json = JSON.stringify({ id: id });
+    axios.post(URL + 'delete.php', json, {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((response) => {
+        const newListWithoutRemoved = items.filter((item) => item.id !== id);
+        setItems(newListWithoutRemoved);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error)
+      });
+  }
+
+  return (
+    <div className='container'>
+      <h1>shopping list</h1>
+      <form onSubmit={save}>
+        <label>add item</label>
+        <input value={item} placeholder='description' onChange={e => setItem(e.target.value)} />
+        <input type='number' value={amount} placeholder='amount' onChange={e => setAmount(e.target.value)} />
+        <button>add</button>
+      </form>
+      <ul>
+        {items?.map(item => (
+          <a class='item' key={item.id}>
+            {item.id && item.description}&nbsp;
+            <a class='amount' key={item.id}>
+              {item.id && item.amount}&nbsp; </a>
+
+            <a href='#' className='delete' onClick={() => remove(item.id)}>delete
+            </a>
+            <br></br>
+          </a>
+
+        ))}
+      </ul>
     </div>
   );
 }
